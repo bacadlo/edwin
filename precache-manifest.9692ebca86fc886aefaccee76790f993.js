4718baf9d06b2530428415566ed132588a0cc210
bacadlo <57646186+bacadlo@users.noreply.github.com>
2020-11-28 18:08:27 -0600

    Updates

---
+++ precache-manifest.9692ebca86fc886aefaccee76790f993.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310e5c14c74a7673978e532bb3da72a9",
+    "url": "/edwin/index.html"
+  },
+  {
+    "revision": "954ba4b9d655264a8a60",
+    "url": "/edwin/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "71995f3235a328614479",
+    "url": "/edwin/static/js/2.3f5965f9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/edwin/static/js/2.3f5965f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954ba4b9d655264a8a60",
+    "url": "/edwin/static/js/main.a846b850.chunk.js"
+  },
+  {
+    "revision": "ffb9421fe8983b79998c",
+    "url": "/edwin/static/js/runtime-main.b1f96783.js"
+  },
+  {
+    "revision": "159b4599eff12990050f17c03289d8d7",
+    "url": "/edwin/static/media/DoubleEd.159b4599.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/edwin/static/media/resume.9d3da0e2.pdf"
+  }
+]);
